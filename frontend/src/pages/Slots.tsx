90333e6fc68256c3bdbe0f9035312980554452a8
unknown <testja039@gmail.com>
2026-04-17 15:24:42 +0200

    Dodane pierdoły pod sloty

--- frontend/src/pages/Slots.tsx
+++ frontend/src/pages/Slots.tsx
@@ -1,16 +1,25 @@
 export const Slots = () => {
   return (
+    <>
     <div className="content">
       <h1>Sloty – Symulator</h1>
-
-      <div className="slot-machine">
-        <div className="reel"></div>
-        <div className="reel"></div>
-        <div className="reel"></div>
+      <div className="slot-machine-new">
+        <div className="slot1">
+          <img src="slots-7.png"/>
+        </div>
+        <div className="slot2">
+          <img src="slots-7.png"/>
+        </div>
+        <div className="slot3">
+          <img src="slots-7.png"/>
+        </div>
       </div>
 
-      <button>Zakręć bębnami</button>
+
     </div>
+
+      <button>Zakręć bębnami</button>
+    </>
   )
 }
 

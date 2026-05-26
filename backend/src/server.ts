fb35f521d64c71867f6b5eb1825d45947a97d510
Krzysztof Foryś <krzys.forys@gmail.com>
2026-05-26 19:37:51 +0200

    Poprawa poradnika (uzupełnienie o bazy). Dodanie struktury backendu i testu czy działa. Na razie skrypt do SQL jest pusty

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,29 @@
 import express from "express";
 import { type Response, type Request } from "express";
 import cors from "cors";
-import {Pool} from "pg";
+import {pool} from "./db";
+
+
+const requiredEnv = [
+  "DB_USER",
+  "DB_HOST",
+  "DB_NAME",
+  "DB_PASSWORD",
+  "DB_PORT",
+];
+
+for(const key of requiredEnv) if(!process.env[key]) throw new Error(`Brak zmiennej środowiskowej ${key}`);
+
 const app = express();
 app.use(cors({origin: 'http://localhost:5173'}))
 app.use(express.json());
 
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "edu-kasyno",
-    password: "postgres",
-    port: 5432
-})
 
-app.listen(3001, () => {
-  console.log('Backend działa na http://localhost:3001');
+
+app.get("/", (reqest: Request, response: Response) => {
+  response.send("Backend śmiga");
+})
+const PORT = Number(process.env.PORT) || 3001;
+app.listen(PORT, () => {
+  console.log(`Backend działa na http://localhost:${PORT}`);
 });

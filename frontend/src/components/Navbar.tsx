985ada1670efedca7ae29975f10afded81ce04bc
MarekL-G <marokomaro.gaska@gmail.com>
2026-05-27 10:43:38 +0200

    ogarniecie balansu dla nav bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom"
-type NavbarProps = {
-  balance: number
-}
-export const Navbar = ({balance}: NavbarProps) => {
-  return (
-      <header>
-        <div className="logo">
-          <span>&#127891;</span>
-          <strong>Edu Kasyno</strong>
-        </div>
+import { useBalance } from "../context/BalanceContext.tsx"
 
-        <nav>
-          <Link to="/" className="link">Home</Link>
-          <Link to="/sloty" className="link">Sloty</Link>
-          <Link to="/ruletka" className="link">Ruletka</Link>
-          <Link to="/zdrapki" className="link">Zdrapki</Link>
-        </nav>
+export const Navbar = () => {
+  const { balance } = useBalance()
+  return (
+    <header>
+      <div className="logo">
+        <span>&#127891;</span>
+        <strong>Edu Kasyno</strong>
+      </div>
+
+      <nav>
+        <Link to="/" className="link">Home</Link>
+        <Link to="/sloty" className="link">Sloty</Link>
+        <Link to="/ruletka" className="link">Ruletka</Link>
+        <Link to="/zdrapki" className="link">Zdrapki</Link>
+      </nav>
 
       <div className="wallet">
         <div>{balance}</div>
-        <img src="zeton-portfel.svg"/>
+        <img src="zeton-portfel.svg" />
       </div>
     </header>
   )
